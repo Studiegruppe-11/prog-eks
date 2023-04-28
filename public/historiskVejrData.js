406515978c022537d30c92bcc4f1261099053b93
d3faulter <nvfbeck@gmail.com>
2023-04-28 13:11:21 +0200

    added

--- public/historiskVejrData.js
+++ public/historiskVejrData.js
@@ -1,42 +1,31 @@
 window.addEventListener("DOMContentLoaded", async () => {
+  async function getData() {
+    let obj;
 
-    async function getData() {
-      let obj;
-  
-      let key = await fetch("key.json");
-      key = await key.json();
-      
-  
-      const res = await fetch(`http://localhost:3000/historiskVejrData`) // api-key er gemt i en json fil, så den er skjult. 
-      // kun US nyheder. 
-  
-      obj = await res.json();
-  
-      return obj
-    }
-  
-    getData();
-  
-    let data = await getData();
+    let key = await fetch("key.json");
+    key = await key.json();
 
-    let date = [];
-    let temp = [];
+    const res = await fetch(`http://localhost:3000/historiskVejrData`); // api-key er gemt i en json fil, så den er skjult.
+    // kun US nyheder.
 
-    console.log(data);
+    obj = await res.json();
 
+    return obj;
+  }
 
+  getData();
 
-    for (let i = 0; i < 30; i++) {
+  let data = await getData();
 
-        date[i] = data[i].date;
-        temp[i] = data[i].temp;
-    
-        document.getElementById(`date${i}`).innerHTML = date[i];
-        document.getElementById(`temp${i}`).innerHTML = temp[i];
+  let date = [];
+  let temp = [];
 
-    }
+  console.log(data[1].date);
 
+  for (let i = 1; i < data.length; i++) {
+    console.log(data[i].date);
+  }
 
-  
-  
+  document.getElementById("date1").innerHTML = date;
+  document.getElementById("date1").innerHTML = date;
 });
