8c1b65b2173d598a3fe7fa18c251cbbed760dc7d
d3faulter <nvfbeck@gmail.com>
2023-05-01 13:46:26 +0200

    rettet

--- routes/news.route.js
+++ routes/news.route.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { executeSQL } = require('../controllers/executeSQL.js');
+const { executeSQL } = require("../controllers/executeSQL.js");
 
-// denne SQL query henter alle news fra databasen ved at bruge executeSQL funktionen fra executeSQL.js. 
-
-router.get('/news', async (req, res) => {
+// GET request handler for fetching news data
+router.get("/news", async (req, res) => {
   try {
-    const result = await executeSQL('SELECT * FROM dbo.News');
-    res.send(result);
+    const result = await executeSQL("SELECT * FROM dbo.News");
+    res.json(result);
   } catch (error) {
     console.log(error);
     res.status(500).send(error.message);
