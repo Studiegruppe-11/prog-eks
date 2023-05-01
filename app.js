e991f59287d0d4e176a58a8329e1ae07cb7ad901
gustav123ze <guze22ab@student.cbs.dk>
2023-05-01 13:31:55 +0200

    ret

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const userRoutes = require("./routes/news.route");
 const weatherRoutes = require("./routes/weather.route");
 
 
-// hvis den linje nedenuder er aktiv fetcher den hele tiden til DB. derfor ikke gør den aktiv
-//const newsToDB = require("./fetcherToDB/newsToDB");
+// // hvis den linje nedenuder er aktiv fetcher den hele tiden til DB. derfor ikke gør den aktiv
+// //const newsToDB = require("./fetcherToDB/newsToDB");
 
+
+// end point til at hente api'er. 
 app.use("/", newsRoutes);
 app.use("/", userRoutes);
 app.use("/", weatherRoutes);
@@ -17,9 +19,11 @@ app.listen(3000, () => {
   console.log("App listening on port 3000");
 });
 
+
+
 // til index.html så vi kan åbne localhost 3000 /index.html
 app.use(express.static("public"));
 
 
-// 11.53
+
 
