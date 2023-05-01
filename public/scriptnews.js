319e2fa1614415276165e10368d6122659709490
gustav123ze <guze22ab@student.cbs.dk>
2023-05-01 14:45:33 +0200

    rettelser

--- public/scriptnews.js
+++ public/scriptnews.js
@@ -26,40 +26,66 @@ window.addEventListener("DOMContentLoaded", async () => {
 
 
 
-let headnews = [];
-let imgnews = [];
-let newspaper = [];
-let urlnews = [];
+  let headnews = [];
+  let imgnews = [];
+  let newspaper = [];
+  let urlnews = [];
+
+
+  // Antallet af nyheder i vores database. (+1 fordi loopet starter på 0)
+  let antalNyheder = (Object.keys(data).length) + 1;
+
+  // kører fra antallet af nyheder - 7 og til antallet af nyheder. altså 7 gange. 
+  for (let i = antalNyheder - 7; i < antalNyheder; i++) {
+
+    headnews[i] = data[(i).toString()].title;
+    newspaper[i] = headnews[i].substr(headnews[i].lastIndexOf("-")).trim();
+    headnews[i] = headnews[i].substr(0, headnews[i].lastIndexOf("-")).trim();
+
+    imgnews[i] = data[(i).toString()].imageUrl;
+    urlnews[i] = data[(i).toString()].url;
+
+    // i html er der lavet 7 divs med id'erne headnews0, headnews1 osv. derfor skal der minus hvilken værdi i har så den passer med id'et. 
+    // og altså starter på 0.
+    document.getElementById(`headnews${i - (antalNyheder - 7)}`).innerHTML = headnews[i];
+    document.getElementById(`imgnews${i - (antalNyheder - 7)}`).innerHTML = '<img src="' + imgnews[i] + '">';
+    document.getElementById(`newspaper${i - (antalNyheder - 7)}`).innerHTML = newspaper[i];
+    document.getElementById(`linknews${i - (antalNyheder - 7)}`).innerHTML = '<a href="' + urlnews[i] + '">Læs mere</a>';
+
+
+  }
+
+
+  // viser de første 7 nyheder med laveste id. oven over viser den de nederste 7 nyheder med højeste id, så de nyeste nyheder vises først.
+
+  // for (let i = 0; i < 7; i++) {
+
+
+  //   headnews[i] = data[(i + 1).toString()].title;
+  //   newspaper[i] = headnews[i].substr(headnews[i].lastIndexOf("-") + 1).trim();
+  //   headnews[i] = headnews[i].substr(0, headnews[i].lastIndexOf("-")).trim();
+
+  //   imgnews[i] = data[(i + 1).toString()].imageUrl;
+  //   urlnews[i] = data[(i + 1).toString()].url;
+
+
+  //   document.getElementById(`headnews${i}`).innerHTML = headnews[i];
+  //   document.getElementById(`imgnews${i}`).innerHTML = '<img src="' + imgnews[i] + '">';
+  //   document.getElementById(`newspaper${i}`).innerHTML = newspaper[i];
+  //   document.getElementById(`linknews${i}`).innerHTML = '<a href="' + urlnews[i] + '">Læs mere</a>';
+
+
+  // }
 
 
 
 
-for (let i = 0; i < 7; i++) {
-
-  headnews[i] = data[(i+1).toString()].title;
-  newspaper[i] = headnews[i].substr(headnews[i].lastIndexOf("-") + 1).trim();
-  headnews[i] = headnews[i].substr(0, headnews[i].lastIndexOf("-")).trim();
-  
-  imgnews[i] = data[(i+1).toString()].imageUrl;
-  urlnews[i] = data[(i+1).toString()].url;
-
-  
-  document.getElementById(`headnews${i}`).innerHTML = headnews[i];
-  document.getElementById(`imgnews${i}`).innerHTML = '<img src="'+imgnews[i]+'">';
-  document.getElementById(`newspaper${i}`).innerHTML = newspaper[i];
-  document.getElementById(`linknews${i}`).innerHTML = '<a href="'+urlnews[i]+'">Læs mere</a>';
-
-
-} 
 
 
 
 
 
-
-
-
-// ALT UNDER HER ER FRA DEN GAMLE OPGAVE
+  // ALT UNDER HER ER FRA DEN GAMLE OPGAVE
 
 
 
@@ -70,20 +96,20 @@ for (let i = 0; i < 7; i++) {
     document.getElementById(`news${i}save`).addEventListener("click", function () {
       if (localStorage.getItem('Success') === 'true') {      // Man skal være logget ind. '
         alert("Nyheden er gemt i favoritter"); // Alert, der siger at den er gemt.
-        
-        user.favoriteArticles.articleTitel =[];
+
+        user.favoriteArticles.articleTitel = [];
         user.favoriteArticles.articleUrl = [];
         user.favoriteArticles.articleImg = [];
         user.favoriteArticles.articleNewspaper = [];
 
-        
+
         user.favoriteArticles.articleTitel[i] = data.articles[i].title;
         user.favoriteArticles.articleUrl[i] = data.articles[i].url;
         user.favoriteArticles.articleImg[i] = data.articles[i].urlToImage;
         user.favoriteArticles.articleNewspaper[i] = data.articles[i].source.name;
-       
 
-    
+
+
         window.localStorage.setItem("user", JSON.stringify(user));
 
 
