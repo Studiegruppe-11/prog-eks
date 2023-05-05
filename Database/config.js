d3baa9dc5d965d78361ae96e1bb397f89421b2b6
d3faulter <nvfbeck@gmail.com>
2023-05-05 23:25:56 +0200

    ny config. Fucker med import i andre filer

---
+++ Database/config.js
@@ -0,0 +1,24 @@
+const { Connection, Request } = require("tedious");
+require("dotenv").config({ path: "../.env" });
+
+const config = {
+  server: process.env.DB_SERVER,
+  authentication: {
+    type: "default",
+    options: {
+      userName: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+  },
+  options: {
+    encrypt: true,
+    database: process.env.DB_NAME,
+  },
+};
+
+console.log(process.env.DB_SERVER);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);
+
+module.exports = { Connection, Request, config };
