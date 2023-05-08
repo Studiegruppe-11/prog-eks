e1be866d9dd8570ea730f7f6dd31e0bdfb9571b3
d3faulter <nvfbeck@gmail.com>
2023-05-08 10:35:36 +0200

    Tilføjet CORS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 require("dotenv").config();
 
 // Importer body-parser modulet for at analysere JSON-data i anmodningens krop
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -22,12 +22,30 @@ app.use(
   })
 );
 
+// ##### CORS START #####
+
+// Lavet for at gå uden om CORS
+// Ideelt set (sikkerhedsmæssigt) skal det være en whitelist med tilladte domæner, men vi har tilføjet alle for simplicitet
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000, http://127.0.0.1:3000"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// ##### CORS SLUT #####
+
 // Importer ruter til bruger, nyheder og vejr fra separate filer
 const newsRoutes = require("./routes/users.route");
 const userRoutes = require("./routes/news.route");
 const weatherRoutes = require("./routes/weather.route");
 
-// Importer moduler til at hente data fra API'er og gemme dem i databasen. De er sat til at køre hver dag kl 14.00. 
+// Importer moduler til at hente data fra API'er og gemme dem i databasen. De er sat til at køre hver dag kl 14.00.
 const newsToDB = require("./fetcherToDB/newsToDB");
 const otherNewsToDB = require("./fetcherToDB/otherNewsToDB");
 const weatherToDB = require("./fetcherToDB/weatherToDB30");
@@ -38,11 +56,10 @@ app.use("/", newsRoutes);
 app.use("/", userRoutes);
 app.use("/", weatherRoutes);
 
-// Lyt på port 3000 og console.log en besked, når serveren er startet. 
+// Lyt på port 3000 og console.log en besked, når serveren er startet.
 app.listen(3000, () => {
   console.log("App listening on port 3000");
 });
-
 
 // Konfigurer Express appen til at servere statiske filer fra public mappen, som indeholder index.html filen
 app.use(express.static("public"));
